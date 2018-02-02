ded0b3538808d3edfc3681134627ca7b490fad91
jc <jc@bl-sice-bl25cm2.ads.iu.edu>
2018-02-02 18:28:36 -0500

    wrapper changed for job submit

--- rappture/nanoconfinement-wrapper.py
+++ rappture/nanoconfinement-wrapper.py
@@ -50,7 +50,7 @@ try:
      #   salt_concentration, '-d', ion_diameter, '-S', simulation_steps, '-f', simulation_params, '-v', 'false'], streamOutput=True)
 	 
 	 exitStatus,stdOutput,stdError = Rappture.tools.executeCommand(
-	 ['submit','--venue','standby@conte','-w','25','-n','10', '--runName',runName, '--tailStdout', '--inputfile','data', 'nanoconfinement-r21',
+	 ['submit','--venue','standby@conte','-w','25','-n','64', '-N','16', '--runName',runName, '--tailStdout', '--inputfile','data', 'nanoconfinement-r21',
 		 '-Z', confinement_length, '-p', positive_valency, '-n', negative_valency, '-c', salt_concentration, 
 		 '-d', ion_diameter, '-S', simulation_steps, '-f', simulation_params, '-v', 'false'], streamOutput=True)
  		 
