49851d8658cb0dd7f7fb92ad1f8dc9ed5e1647f3
Vikram Jadhao <vjadhao@login1.bigred2.uits.iu.edu>
2018-11-14 16:29:38 -0500

    testing changes on PR

--- src/interface.h
+++ src/interface.h
@@ -60,7 +60,7 @@ class INTERFACE
   void set_up(double, double, int, int, double, double, double);
   void put_saltions_inside(vector<PARTICLE>&, int, int, double, double, vector<PARTICLE>&);
   void discretize(double, double);
-  void lammps(vector<PARTICLE>&, int, int, vector<PARTICLE>&, double);
+  void generate_lammps_datafile(vector<PARTICLE>&, int, int, vector<PARTICLE>&, double);
 
   INTERFACE(VECTOR3D initial_posvec = VECTOR3D(0,0,0), double initial_ein = 1, double initial_eout = 1)
   {
